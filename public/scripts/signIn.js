865a4203e2d1608ad48e558dbf0af68990a9bc48
John Shelnutt <harrisonshelnutt@gmail.com>
2020-02-13 13:35:18 -0600

    Added sign in page and client side functionality
    (sprint 2, tasks 2 and 3)

--- public/scripts/signIn.js
+++ public/scripts/signIn.js
@@ -3,6 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function validateForm() {
-	// TODO: Validate the user input
-	return true;
+	let employeeID = document.forms["signInForm"]["employee_id"].value;
+	let password = document.forms["signInForm"]["password"].value;
+
+	// Verify neither field is blank and employeeID is numeric
+	if (employeeID !== "" && !isNaN(employeeID) && password !== "") {
+		return true;
+	} else {
+		let errorMessage = "Both fields must be filled out and Employee ID must be numeric";
+		document.getElementById("error").style.display = "block";
+		document.getElementById("errorMessage").innerHTML = errorMessage;
+		return false;
+	}
+
 }
