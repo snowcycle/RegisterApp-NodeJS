6fe60dc4cda801c1ca17d4cb7a103e629c55f924
Fernando Arreola Garcia <farreola@uark.edu>
2020-02-29 17:18:55 -0600

    adding employeeQuery functionality

--- src/controllers/commands/employees/employeeQuery.ts
+++ src/controllers/commands/employees/employeeQuery.ts
@@ -1,3 +1,21 @@
-export class EmployeeQuery {
-    
+import { CommandResponse, Employee } from "../../typeDefinitions";
+import { EmployeeModel } from "../models/employeeModel";
+import * as Helper from "../helpers/helper";
+import * as EmployeeRepository from "../models/employeeModel";
+import { Resources, ResourceKey } from "../../../resourceLookup";
+
+export const queryById = async (employeeRecordId: number): Promise<CommandResponse<Employee>> => {
+if (Helper.isBlankString(employeeRecordId.toString())) {
+	return Promise.reject(<CommandResponse<Employee>>{
+		status: 422,
+		message: Resources.getString(ResourceKey.EMPLOYEE_RECORD_ID_INVALID)
+	});
 }
+
+return EmployeeRepository.queryById(<string>EmployeeId)
+	.then((queriedEmployee: (EmployeeModel | null)): Promise<CommandResponse<Employee>> => {
+		if (queriedEmployee == null) {
+			return Promise.reject(<CommandResponse<>>)
+		}
+	}
+};
