5f737e44818ce17eef058baedfe24c552d0cfcb3
Fernando Arreola <farreola@uark.edu>
2020-03-04 19:19:05 -0600

    Adding employeeQuery functionality. Adding strange file appearing as an untracked file to .gitignore

--- src/controllers/commands/employees/employeeQuery.ts
+++ src/controllers/commands/employees/employeeQuery.ts
@@ -1,21 +1,31 @@
 import { CommandResponse, Employee } from "../../typeDefinitions";
 import { EmployeeModel } from "../models/employeeModel";
 import * as Helper from "../helpers/helper";
+import * as EmployeeHelper from "./helpers/employeeHelper";
 import * as EmployeeRepository from "../models/employeeModel";
 import { Resources, ResourceKey } from "../../../resourceLookup";
 
-export const queryById = async (employeeRecordId: number): Promise<CommandResponse<Employee>> => {
-if (Helper.isBlankString(employeeRecordId.toString())) {
-	return Promise.reject(<CommandResponse<Employee>>{
-		status: 422,
-		message: Resources.getString(ResourceKey.EMPLOYEE_RECORD_ID_INVALID)
-	});
-}
 
-return EmployeeRepository.queryById(<string>EmployeeId)
-	.then((queriedEmployee: (EmployeeModel | null)): Promise<CommandResponse<Employee>> => {
-		if (queriedEmployee == null) {
-			return Promise.reject(<CommandResponse<>>)
-		}
+export const queryById = async (employeeRecordId?: string): Promise<CommandResponse<Employee>> => {
+	if (Helper.isBlankString(employeeRecordId)) {
+		return Promise.reject(<CommandResponse<Employee>>{
+			status: 422,
+			message: Resources.getString(ResourceKey.EMPLOYEE_RECORD_ID_INVALID)
+		});
 	}
+
+	return EmployeeRepository.queryById(<string>employeeRecordId)
+		.then((queriedEmployee: (EmployeeModel | null)): Promise<CommandResponse<Employee>> => {
+			if (queriedEmployee == null) {
+				return Promise.reject(<CommandResponse<Employee>>{
+					status: 404,
+					message: Resources.getString(ResourceKey.EMPLOYEE_NOT_FOUND)
+				});
+			}
+
+			return Promise.resolve(<CommandResponse<Employee>>{
+				status: 200,
+				data: EmployeeHelper.mapEmployeeData(queriedEmployee)
+			});
+		});
 };
