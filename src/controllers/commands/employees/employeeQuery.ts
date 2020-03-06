9f3f14fc0166a4e491b8ff4d9836bc871230b747
jtm029 <43485929+jtm029@users.noreply.github.com>
2020-03-05 22:30:33 -0600

    show buttons based on elevated user

--- src/controllers/commands/employees/employeeQuery.ts
+++ src/controllers/commands/employees/employeeQuery.ts
@@ -1,3 +1,17 @@
+import { EmployeeModel } from "../models/employeeModel";
+import { CommandResponse } from "../../typeDefinitions";
+import * as EmployeeRepository from "../models/employeeModel";
+import * as EmployeeHelper from "../employees/helpers/employeeHelper";
 export class employeeQuery {
     
 }
+
+export const isElevatedUser = async (): Promise<CommandResponse<boolean>> => {
+	return EmployeeRepository.queryActiveExists()
+		.then((queriedEmployee: EmployeeModel | null): CommandResponse<boolean> => {
+			return <CommandResponse<boolean>>{
+				status: 200,
+				data: EmployeeHelper.isElevatedUser(queriedEmployee)
+			};
+		});
+};
--- src/controllers/commands/employees/helpers/employeeHelper.ts
+++ src/controllers/commands/employees/helpers/employeeHelper.ts
@@ -1,9 +1,32 @@
 import { EmployeeClassification } from "../../models/constants/entityTypes";
+import { EmployeeModel } from "../../models/employeeModel";
+import { Employee } from "../../../typeDefinitions";
 
 export const hashString = (toHash: string): string => {
 	return ""; // TODO: Look at https://nodejs.org/docs/latest-v12.x/api/crypto.html#crypto_crypto_createhash_algorithm_options as one option
 };
 
-export const isElevatedUser = (employeeClassification: EmployeeClassification): boolean => {
-	return false; // TODO: Determine if an employee is an elevated user by their classification
+export const isElevatedUser = (employee: EmployeeModel | null): boolean => {
+	if(employee){
+		if(employee.classification === EmployeeClassification.GeneralManager)
+			return true;
+		else if(employee.classification === EmployeeClassification.ShiftManager)
+			return true;
+		else
+		return false;
+	}
+		return false;
+};
+
+export const mapEmployeeData = (queriedProduct: EmployeeModel): Employee => {
+	return <Employee>{
+		id: queriedProduct.id,
+		active: queriedProduct.active,
+		lastName: queriedProduct.lastName,
+		createdOn: queriedProduct.createdOn,
+		firstName: queriedProduct.firstName,
+		managerId: queriedProduct.managerId,
+		employeeId: queriedProduct.employeeId.toString(),
+		classification: queriedProduct.classification
+	};
 };
